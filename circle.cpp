78482134e1b73f2cef8578897ca85a86b0c6cd39
dep24 <dep24@yandex.ru>
2017-10-17 11:06:50 +0300

    implemented circle

---
+++ circle.cpp
@@ -0,0 +1,43 @@
+#include "circle.h"
+
+using namespace std;
+void Circle::getInfo() {
+    cout<<"r = "<<r<<"   teta="<<teta<<"  fi= "<<fi<<endl;
+}
+
+Circle::Circle() {}
+
+Circle::Circle(double r, double teta, double fi) {
+  setR(r);
+    setTeta(teta);
+    setFi(fi);
+}
+
+double Circle::getR() const {
+    return r;
+}
+
+void Circle::setR(double r) {
+    if (r<0) r=0;
+    Circle::r = r;
+}
+
+double Circle::getTeta() const {
+    return teta;
+}
+
+void Circle::setTeta(double teta) {
+    if (teta<0) teta=0;
+    Circle::teta = teta;
+}
+
+double Circle::getFi() const {
+    return fi;
+}
+
+void Circle::setFi(double fi) {
+    if (fi<0) fi=0;
+    Circle::fi = fi;
+}
+
+
--- circle.h
+++ circle.h
@@ -1,8 +1,32 @@
 //
 // Created by M17-612 on 17.10.2017.
 //
-
+#include <iostream>
 #ifndef PRACTIC1_CIRCLE_H
 #define PRACTIC1_CIRCLE_H
 
+class Circle {
+private:
+    double r;
+    double teta;
+    double fi;
+public:
+    void getInfo();
+
+    double getR() const;
+
+    void setR(double r);
+
+    double getTeta() const;
+
+    void setTeta(double teta);
+
+    double getFi() const;
+
+    void setFi(double fi);
+
+    Circle();
+
+    Circle(double r, double teta, double fi);
+};
 #endif //PRACTIC1_CIRCLE_H
